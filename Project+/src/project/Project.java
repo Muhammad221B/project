951cb40d0d788341a438eb56a40c90d5a58c18e5
Muhammad221B <168672825+Muhammad221B@users.noreply.github.com>
2024-05-02 15:36:00 +0300

    Add files via upload

---
+++ Project+/src/project/Project.java
@@ -0,0 +1,16 @@
+package project;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class Project {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
